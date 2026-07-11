dcb5523c31a558794404f45f8f51e91cc52e9454
inkfinit <inkfinit.dev@gmail.com>
2026-07-12 04:09:32 +0700

    second  setup

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
-</template>
-<template>
-  <div>
     <VitePwaManifest />
     <NuxtRouteAnnouncer />
     <NuxtWelcome /> </div>
